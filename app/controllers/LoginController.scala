e64b07723488867a8321be9901b596a6e292718c
Joseph Simmons <jwmsimmons@gmail.com>
2019-07-26 10:14:06 +0100

    Joe

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -4,25 +4,25 @@ import javax.inject.Inject
 import models.Login
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
-import loginPage._
 
-class LoginController @Inject()(val messagesApi: MessagesApi, environment: play.api.Environment) extends Controller with I18nSupport{
+class LoginController @Inject()(val messagesApi: MessagesApi, environment: play.api.Environment) extends Controller with I18nSupport {
 
-  def getLoginPage() = Action {implicit request: =>
+  def getLoginPage() = Action { implicit request: =>
     Ok(views.html.loginPage(Login.loginForm))
   }
 
-  def postLoginForm()= Action {implicit request =>
+  def postLoginForm() = Action { implicit request =>
     Login.loginForm.bindFromRequest.fold({ badLoginForm =>
       BadRequest(views.html.loginPage(badLoginForm))
-    },{login =>
-      if(Login.userList.contains(login))
+    }, { login =>
+      if (Login.userList.contains(login))
         Redirect("/portfolio")
       else {
         Login.userList.foreach(println)
         Login.userList.append(login)
         Redirect("/portfolio")
-      }    })
+      }
+    })
   }
 
 }
--- app/models/Login.scala
+++ app/models/Login.scala
@@ -1,23 +1,17 @@
 package models
 
 import play.api.data.Form
-import play.api.data.Form
-import scala.collection.mutable.ArrayBuffer
 
-case class Login {username: String, password: String)
-  {
+case class Login (username: String, password: String)
 
-    object Login {
-      val loginForm = Form(
-        mapping(
-          "username" -> nonEmptyText,
-          "password" -> nonEmptyText
-        )
-        (Longin.apply)(Login.unapply)
+  object Login {
+    val loginForm = Form(
+      mapping(
+        "username" -> nonEmptyText,
+        "password" -> nonEmptyText
       )
-    }
-
-    val arrayBuffer
+      (Login.apply)(Login.unapply)
+    )
   }
 
-}
+
